606a4928a176a90da75ffebab44f9c1e5f7e8efa
Austinobravo <Austinobravo@gmail.com>
2024-11-21 17:24:53 +0100

    MAde the nav responsive

--- components/globals/Nav.tsx
+++ components/globals/Nav.tsx
@@ -6,7 +6,7 @@ import { Coffee } from 'lucide-react';
 
 const Nav = () => {
   return (
-    <nav className='flex justify-between py-6 border-b-2 border-color bg-white md:px-12 px-6 fixed w-full z-20'>
+    <nav className='flex justify-between gap-3 md:flex-row flex-col py-6 border-b-2 border-color bg-white md:px-12 px-6 fixed w-full z-20'>
       <Link href={`/`} title='Mailer Logo'>
         <h2 className='text-2xl font-bold text-blue-700' >Mailer<sup className='pl-1 text-xs font-normal text-muted-foreground'>by xnyder</sup></h2>
       </Link>
